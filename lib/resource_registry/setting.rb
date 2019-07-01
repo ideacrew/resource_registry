cabeb73337cd611dc227c1e9de7e9b4db0306167
Dan Thomas <dan.thomas@dc.gov>
2019-07-01 18:27:47 -0400

    Nest non-identifier attributes under meta tag.  Specify more appropriate data types and rules for attributes.

--- lib/resource_registry/setting.rb
+++ lib/resource_registry/setting.rb
@@ -1,14 +1,22 @@
 require 'dry-struct'
 
 module ResourceRegistry
-    class Setting < Dry::Struct
+  class Setting < Dry::Struct
+    transform_keys(&:to_sym)
 
-      attribute :key,         Types::Symbol
-      attribute :title,       Types::String
-      attribute :description, Types::Strict::String
-      attribute :type,        Types::Symbol
-      attribute :value,       Types::Coercible::String
-      attribute :default,     Types::Coercible::String
+    # Collection set will often resolve to an independent system component
+    # Setting identifier must be unique for each :namespace/:key combination
+    attribute :collection_set_name, Types::Symbol
+    attribute :namespace?,          Types::Symbol
+    attribute :key,                 Types::Symbol
 
+    attribute :meta do
+      attribute :title,             Types::Strict::String
+      attribute :description,       Types::Strict::String
+      attribute :type,              Types::Symbol
+      attribute :default,           Types::Coercible::String
+      attribute :value,             Types::Coercible::String
     end
+
+  end
 end
--- spec/resource_registry/setting_spec.rb
+++ spec/resource_registry/setting_spec.rb
@@ -2,20 +2,26 @@ require "spec_helper"
 
 RSpec.describe ResourceRegistry::Setting do
 
-  let(:key)           { :copyright_period_start_year }
-  let(:title)         { "Copyright Period Start Year"}
-  let(:description)   { "The initial calendar year (YYYY) that site content copyright is asserted" }
-  let(:type)          { :string }
-  let(:value)         { 2018 }
-  let(:default)       { "::TimeKeeper.date_of_record.year" }
+  let(:collection_set_name)   { :enroll_app }
+  let(:namespace)             { :ui }
+  let(:key)                   { :copyright_period_start_year }
+  let(:title)                 { "Copyright Period Start Year"}
+  let(:description)           { "The initial calendar year (YYYY) that site content copyright is asserted" }
+  let(:type)                  { :string }
+  let(:value)                 { "2018" }
+  let(:default)               { "::TimeKeeper.date_of_record.year" }
 
   let(:params) do {
+      collection_set_name: collection_set_name,
+      namespace: namespace,
       key: key,
-      title: title,
-      description: description,
-      type: type,
-      value: value,
-      default: default,
+      meta: {
+        title: title,
+        description: description,
+        type: type,
+        value: value,
+        default: default,        
+      },
     }
   end
 
@@ -23,8 +29,30 @@ RSpec.describe ResourceRegistry::Setting do
 
 
   describe "Attributes and restrictions" do
-
     it { expect(subject).to be_a Dry::Struct }
+
+    it { expect(subject[:collection_set_name]).to eq collection_set_name }
+    it { expect(subject[:namespace]).to eq namespace }
+    it { expect(subject[:key]).to eq key }
+
+    it { expect(subject[:meta][:title]).to eq title }
+    it { expect(subject[:meta][:description]).to eq description }
+    it { expect(subject[:meta][:type]).to eq type }
+    it { expect(subject[:meta][:default]).to eq default }
+    it { expect(subject[:meta][:value]).to eq value }
+
+    context "without an optional namespace attribute value" do
+      subject { described_class.new(params.except(:namespace).valid?).to be_truthy }
+    end
+
+    # context "without a title paramater" do
+    #   subject { described_class.new(params.tap { |p| p[:meta].tap { |m| m.delete(:title) } }).valid?  }
+    #   subject { described_class.new(params.tap { |p| p[:meta].tap { |m| m.delete(:title) } }) }
+
+    #   it { expect(subject[:meta][:title]).to eq subject }
+
+
+    # end
 
   end
 
