d3cd7e9008b8788f5dba56f46e12f9056d187457
Raghuram Ganjala <raghuramg83@gmail.com>
2019-07-31 14:03:48 -0400

    Updated boot

--- lib/system/boot.rb
+++ lib/system/boot.rb
@@ -2,11 +2,11 @@ require 'dry/system/container'
 
 module ResourceRegistry
   
-  Container = Dry::System::Container
+  Container = Class.new(Dry::System::Container)
 
   Kernel.const_set("Registry", ResourceRegistry::Container)
   ResourceRegistry.const_set(:RegistryInject, Registry.injector)
 
   require_relative 'local/transactions'
-  require_relative 'local/operations'  
+  require_relative 'local/operations'
 end
