a62c678db9c90117a911d792e109f8774069b593
Dan Thomas <dan@ideacrew.com>
2020-02-11 09:17:16 -0500

    Add file read command

---
+++ lib/resource_registry/stores/file/read.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module ResourceRegistry
+  module Stores
+    module File
+
+      # Open and read contents of a file
+      class Read
+        send(:include, Dry::Monads[:result, :do])
+
+        # @param [String] params Full file pathname
+        # @return [String] File contents wrapped in Dry::Monads::Result
+        def call(params)
+          contents = yield load(params)
+          Success(contents)
+        end
+
+        private
+
+        def load(params)
+          begin
+            Success(::File.read(params.to_s))
+          rescue Errno::ENOENT
+            Failure["No such file or directory", params: params]
+          end
+        end
+
+      end
+    end
+  end
+end
