3f892199cde3181f33e43356baad6a535e4c3392
jayreddy519 <jayanth.rordev@gmail.com>
2025-07-02 09:43:28 -0400

    fixed syntx error

--- lib/resource_registry/setting.rb
+++ lib/resource_registry/setting.rb
@@ -27,49 +27,48 @@ module ResourceRegistry
     attribute :meta,    ResourceRegistry::Meta.optional.meta(omittable: true)
 
     # Override accessor to normalize date range strings
-      def item
-        @normalized_item ||= convert_range_strings(self[:item])
-      end
+    def item
+      @normalized_item ||= convert_range_strings(self[:item])
+    end
 
-      private
+    private
 
-      def convert_range_strings(value)
-        return value if value.is_a?(Range)
+    def convert_range_strings(value)
+      return value if value.is_a?(Range)
 
-        if value.is_a?(String) && value.include?("..")
-          begin_str, end_str = value.split("..").map(&:strip)
+      if value.is_a?(String) && value.include?("..")
+        begin_str, end_str = value.split("..").map(&:strip)
 
-          # Only try to parse if both ends look like dates
-          if looks_like_date?(begin_str) && looks_like_date?(end_str)
-            begin_date = parse_date(begin_str)
-            end_date = parse_date(end_str)
-            return (begin_date..end_date) if begin_date && end_date
-            return nil
-          end
-          return value
+        # Only try to parse if both ends look like dates
+        if looks_like_date?(begin_str) && looks_like_date?(end_str)
+          begin_date = parse_date(begin_str)
+          end_date = parse_date(end_str)
+          return (begin_date..end_date) if begin_date && end_date
+          return nil
         end
-        value
+        return value
       end
+      value
+    end
 
-      def looks_like_date?(str)
-        [
-          /^\d{4}-\d{1,2}-\d{1,2}$/,
-          /^\d{4}\/\d{1,2}\/\d{1,2}$/,
-          /^\d{1,2}\/\d{1,2}\/\d{4}$/
-        ].any? { |re| str.match?(re) }
-      end
+    def looks_like_date?(str)
+      [
+        /^\d{4}-\d{1,2}-\d{1,2}$/,
+        /^\d{4}\/\d{1,2}\/\d{1,2}$/,
+        /^\d{1,2}\/\d{1,2}\/\d{4}$/
+      ].any? { |re| str.match?(re) }
+    end
 
-      def parse_date(str)
-        case str
-        when /^\d{4}-\d{1,2}-\d{1,2}$/
-          Date.strptime(str, "%Y-%m-%d")
-        when /^\d{4}\/\d{1,2}\/\d{1,2}$/
-          Date.strptime(str, "%Y/%m/%d")
-        when /^\d{1,2}\/\d{1,2}\/\d{4}$/
-          Date.strptime(str, "%m/%d/%Y")
-        else
-          nil
-        end
+    def parse_date(str)
+      case str
+      when /^\d{4}-\d{1,2}-\d{1,2}$/
+        Date.strptime(str, "%Y-%m-%d")
+      when /^\d{4}\/\d{1,2}\/\d{1,2}$/
+        Date.strptime(str, "%Y/%m/%d")
+      when /^\d{1,2}\/\d{1,2}\/\d{4}$/
+        Date.strptime(str, "%m/%d/%Y")
+      else
+        nil
       end
     end
   end
